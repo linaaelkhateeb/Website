6f108688365fa12b67a2321380f90b55623fd5b0
linaaelkhateeb <lina2302249@miuegypt.edu.eg>
2025-04-07 12:33:56 +0200

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,1 +1,12 @@
-test
+function toggleReadMore(button) {
+            const card = button.closest('.attraction-card');
+            const content = card.querySelector('.read-more-content');
+            
+            button.classList.toggle('active');
+            
+            if (content.style.maxHeight) {
+                content.style.maxHeight = null;
+            } else {
+                content.style.maxHeight = content.scrollHeight + 'px';
+            }
+        }
