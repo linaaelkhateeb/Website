3e1274dd1218818b0870ee532b72d5e9ff38072e
danaes25 <danasameh44@gmail.com>
2025-04-08 11:21:53 +0200

    Update reserve.js

--- reserve.js
+++ reserve.js
@@ -2,17 +2,17 @@ document.addEventListener("DOMContentLoaded", function () {
     const reservationForm = document.getElementById("reservation-form");
   
     reservationForm.addEventListener("submit", function (e) {
-      e.preventDefault(); // Prevent actual form submission
+      e.preventDefault(); 
   
       const name = document.getElementById("name").value;
       const date = document.getElementById("date").value;
       const time = document.getElementById("time").value;
       const people = document.getElementById("people").value;
   
-      // Fun confirmation alert
+      
       alert(`🎉 Reservation Confirmed! 🎉`);
   
-      reservationForm.reset(); // Optional: clear the form after submission
+      reservationForm.reset(); 
     });
   });
   
