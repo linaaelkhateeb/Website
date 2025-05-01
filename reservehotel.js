60e52b3dddf1273d9097d4b29300c0a6107ee793
danaes25 <danasameh44@gmail.com>
2025-05-01 20:38:07 +0300

    Delete reservehotel.js

--- reservehotel.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("hotel-reservation-form").addEventListener("submit", function (e) {
-    e.preventDefault();
-    alert("🎉 Your hotel reservation has been confirmed! ");
-    this.reset(); 
-});
