8caa84b7b117ee3c6e8403c7f3adf17952c3bdff
SalmaMIUCS <salma2304455@miuegypt.edu.eg>
2025-06-16 00:39:03 +0300

    up

--- travel-app/routes/bookings.js
+++ travel-app/routes/bookings.js
@@ -4,5 +4,9 @@ const bookingController = require('../controllers/bookingController');
 const { ensureAuth } = require('../middleware/auth');
 
 router.post('/', ensureAuth, bookingController.bookTrip);
+router.get('/mybookings', ensureAuth, async (req, res) => {
+  const bookings = await Booking.find({ user: req.user._id }).populate('trip');
+  res.render('mybookings', { bookings });
+});
 
 module.exports = router;
